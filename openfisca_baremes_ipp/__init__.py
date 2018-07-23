d31c9966c1e68dd6b344ff08597a2c3a04a97bb2
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-07-23 18:40:24 -0400

    Don't rely on country template

--- openfisca_baremes_ipp/__init__.py
+++ openfisca_baremes_ipp/__init__.py
@@ -3,15 +3,20 @@
 import os
 
 from openfisca_core.taxbenefitsystems import TaxBenefitSystem
+from openfisca_core.entities import build_entity
 
-from openfisca_country_template import entities
-
+Person = build_entity(
+    key = "person",
+    plural = "persons",
+    label = u'Person',
+    is_person = True,
+    )
 
 COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class CountryTaxBenefitSystem(TaxBenefitSystem):
     def __init__(self):
-        super(CountryTaxBenefitSystem, self).__init__(entities.entities)
+        super(CountryTaxBenefitSystem, self).__init__([Person])
         param_path = os.path.join(COUNTRY_DIR, 'parameters')
         self.load_parameters(param_path)
