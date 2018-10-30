17f220e977cc03fd69c05400f47be05bb43b7f20
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-10-29 20:27:09 -0400

    Add dummy variable to allow core upgrade

--- openfisca_baremes_ipp/__init__.py
+++ openfisca_baremes_ipp/__init__.py
@@ -3,6 +3,7 @@
 import os
 
 from openfisca_core.taxbenefitsystems import TaxBenefitSystem
+from openfisca_core.model_api import Variable, MONTH
 from openfisca_core.entities import build_entity
 
 Person = build_entity(
@@ -15,8 +16,15 @@ Person = build_entity(
 COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
+class dummy_variable(Variable):
+    entity = Person
+    value_type = float
+    definition_period = MONTH
+
+
 class CountryTaxBenefitSystem(TaxBenefitSystem):
     def __init__(self):
         super(CountryTaxBenefitSystem, self).__init__([Person])
         param_path = os.path.join(COUNTRY_DIR, 'parameters')
         self.load_parameters(param_path)
+        self.add_variable(dummy_variable)
